da9ce33289a76f9c5a282eb92fe9361980209b1f
simo blues <wambuisimon712@gmail.com>
2024-01-06 17:50:41 +0300

    changes

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,43 +1,57 @@
-import Link from "next/link";
+"use client"
 import NavLink from "./navLink.jsx/NavLink";
-
+import styles from './links.module.css'
+import { useState } from "react";
 const Links = () => {
-  const links = [
-    {
-      title: "Homepage",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contacts",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false)
 
-  //temporary
-  const session = true
-  const isAdmin = true
+const links = [
+  {
+    title: "Homepage",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contacts",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+
+//temporary
+const session = true
+const isAdmin = true
 
   return (
-    <div>
+    <div className={styles.container}>
+    <div className={styles.links}>
       {links.map((link) => (
         <NavLink item={link} key={link.title} />
       ))}
       {session ? (
         <>
         {isAdmin && <NavLink item={{ title: "Admin", path: "/admin"}} />}
-        <button>Logout</button>
+        <button className={styles.logout}>Logout</button>
         </>
       ) : (
         <NavLink item={{ title: "Login", path: "/login"}} />
       )}
+    </div>
+    <button className={styles.menuButton} onClick={() => setOpen((prev) => !prev)}>menu</button>
+     {
+      open && <div className={styles.mobileLinks}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+      </div>
+     }
     </div>
   );
 };
