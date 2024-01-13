71b1974d2c8284cd9fd7652faec6d1b39d986382
simo blues <wambuisimon712@gmail.com>
2024-01-13 21:51:10 +0300

    changes

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -25,7 +25,7 @@ AOS.init({
   duration: 400, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
+  mirror: false,
   anchorPlacement: 'top-bottom', 
 
 });
