41c6f32112a5eedf976c432062ab06806bdaba3c
simo blues <wambuisimon712@gmail.com>
2024-01-15 20:33:47 +0300

    changes

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -15,14 +15,14 @@ AOS.init({
   animatedClassName: 'aos-animate', // class applied on animation
   useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  debounceDelay: 100, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
   
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 400,
+  duration: 600,
   easing: 'ease',
   once: false,
   mirror: false,
@@ -36,10 +36,12 @@ const Home = () => {
     <Navbar />
     <div className='home'>
       <div className='softwareIntro'>
-        <div className='wordIntro'>
-          <h1>
+        <div className='wordIntro' data-aos="fade-down">
+          <h1>blueHike</h1>
+          <h2>
           my name is bluehike, a passionate Software developer and a photographer.
-          </h1>
+          </h2>
+          <h3></h3>
           </div>
         <div className='dpIntro'>
           <img src='simo.jpg' />
@@ -48,7 +50,7 @@ const Home = () => {
       <div className='projectIntro'></div>
       <div className='photoIntro'>
         <div className='bg-image'></div>
-        <div className='bg-text'>Hello</div>
+        <div className='bg-text' data-aos="fade-down">Hello world. welcome to my worlds</div>
       </div>
     </div>
       
