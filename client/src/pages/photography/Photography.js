88c7e2bc1668dfad2bcd35f9264335eb5efce96d
simo blues <wambuisimon712@gmail.com>
2024-01-17 20:19:37 +0300

    changes

--- client/src/pages/photography/Photography.js
+++ client/src/pages/photography/Photography.js
@@ -18,61 +18,25 @@ const Photography = () => {
           </div>
           <div className="bg-text2">
             <div className="leftbg">hello
-            bluehike
-            bluehike
-            bluehike
-            bluehike
-            bluehike
-            bluehike
-            bluehike
-            bluehike
-            In order to serve our resources to you at lightning speed, we store all of our software on a high-performance cloud-based storage server. What gives GetIntoPC a competitive edge against others? We don’t just provide software! We have a large library of easy to follow tutorials to get you up and running as quickly as possible. Our guides are easy to follow, so don’t fret! All sorts of software is available on GetIntoPC. Ranging from CAD software to operating systems, we have it all!
-
-
-
-
-
-To mitigate issues with our software, we run rigorous tests on all executables posted to this platform. Not only that, but we make sure each and every file is clean and virus-free. Once we are sure a program runs properly, it gets posted. Furthermore, you can find tutorials on how to configure settings and learn tips and tricks to optimize your system’s performance. All of our tutorials are super easy to follow along, with helpful screenshots and tips along the way.
-
-With the rise of cyber threats in recent years, we make sure that ALL of our resources are clean. We scan every file uploaded to our platform, and make sure it has no malware embedded in it. Why? Your safety is our number one priority! Don’t believe us? Check it out for yourself!
-
-If you notice any broken links, please do not hesitate to contact one of our staff members. We will take action immediately.
-
-Credits: Sam
+        
             </div>
             <div className="leftbg">hello
-            In order to serve our resources to you at lightning speed, we store all of our software on a high-performance cloud-based storage server. What gives GetIntoPC a competitive edge against others? We don’t just provide software! We have a large library of easy to follow tutorials to get you up and running as quickly as possible. Our guides are easy to follow, so don’t fret! All sorts of software is available on GetIntoPC. Ranging from CAD software to operating systems, we have it all!
-
-
-
-
-
-To mitigate issues with our software, we run rigorous tests on all executables posted to this platform. Not only that, but we make sure each and every file is clean and virus-free. Once we are sure a program runs properly, it gets posted. Furthermore, you can find tutorials on how to configure settings and learn tips and tricks to optimize your system’s performance. All of our tutorials are super easy to follow along, with helpful screenshots and tips along the way.
-
-With the rise of cyber threats in recent years, we make sure that ALL of our resources are clean. We scan every file uploaded to our platform, and make sure it has no malware embedded in it. Why? Your safety is our number one priority! Don’t believe us? Check it out for yourself!
-
-If you notice any broken links, please do not hesitate to contact one of our staff members. We will take action immediately.
-
-Credits: Sam
-In order to serve our resources to you at lightning speed, we store all of our software on a high-performance cloud-based storage server. What gives GetIntoPC a competitive edge against others? We don’t just provide software! We have a large library of easy to follow tutorials to get you up and running as quickly as possible. Our guides are easy to follow, so don’t fret! All sorts of software is available on GetIntoPC. Ranging from CAD software to operating systems, we have it all!
-
-
-
-
-
-To mitigate issues with our software, we run rigorous tests on all executables posted to this platform. Not only that, but we make sure each and every file is clean and virus-free. Once we are sure a program runs properly, it gets posted. Furthermore, you can find tutorials on how to configure settings and learn tips and tricks to optimize your system’s performance. All of our tutorials are super easy to follow along, with helpful screenshots and tips along the way.
-
-With the rise of cyber threats in recent years, we make sure that ALL of our resources are clean. We scan every file uploaded to our platform, and make sure it has no malware embedded in it. Why? Your safety is our number one priority! Don’t believe us? Check it out for yourself!
-
-If you notice any broken links, please do not hesitate to contact one of our staff members. We will take action immediately.
-
-Credits: Sam
             </div>
-          
           </div>
-         
          </div>
-        <div className="parallax-container"></div> 
+         <div className="background">hello</div>
+        <div className="parallax-container">
+          {/* <img src="simo.jpg" /> */}
+          </div> 
+        <div style={{height:"1000px", backgroundColor:"brown",fontSize:"36px"}}>
+          Scroll Up and Down this page to see the parallax scrolling effect.
+          This div is just here to enable scrolling.
+          Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+        <div className="parallax-container">
+        {/* <img src="simo.jpg" /> */}
+
+        </div>
       </div>
       <Footer />
     </div>
