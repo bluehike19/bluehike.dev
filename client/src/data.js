be95822470dcafd4a05a3004fb7e5936891e8adc
simo blues <wambuisimon712@gmail.com>
2024-01-18 13:29:33 +0300

    changes

--- client/src/data.js
+++ client/src/data.js
@@ -1,3 +1,49 @@
-export default project = {
-    id: ''
-}
+// data.js
+
+const projectData = [
+    {
+      id: 1,
+      img: 'https://placekitten.com/200/300', // replace with your image URLs
+      title: 'Card 1',
+      desc: 'Description for Card 1',
+      link: 'https://example.com/card1',
+    },
+    {
+      id: 2,
+      img: 'https://placekitten.com/201/300',
+      title: 'Card 2',
+      desc: 'Description for Card 2',
+      link: 'https://example.com/card2',
+    },
+    {
+      id: 3,
+      img: 'https://placekitten.com/202/300',
+      title: 'Card 3',
+      desc: 'Description for Card 3',
+      link: 'https://example.com/card3',
+    },
+    {
+      id: 4,
+      img: 'https://placekitten.com/203/300',
+      title: 'Card 4',
+      desc: 'Description for Card 4',
+      link: 'https://example.com/card4',
+    },
+    {
+      id: 5,
+      img: 'https://placekitten.com/204/300',
+      title: 'Card 5',
+      desc: 'Description for Card 5',
+      link: 'https://example.com/card5',
+    },
+    {
+      id: 6,
+      img: 'https://placekitten.com/205/300',
+      title: 'Card 6',
+      desc: 'Description for Card 6',
+      link: 'https://example.com/card6',
+    },
+  ];
+  
+  export default projectData;
+  
