3f4ca04fdf7689731537acecbc98ed336896c049
simo blues <wambuisimon712@gmail.com>
2024-01-18 18:58:23 +0300

    changes

--- client/src/components/project/Project.js
+++ client/src/components/project/Project.js
@@ -4,15 +4,19 @@ import React from 'react'
 
 const Project = () => {
   return (
-    <div>
+    <div className="projects">
     {projectData.map((card) => (
       <div key={card.id} className="card">
+        <div className="imageCard">
         <img src={card.img} alt={card.title} />
+        </div>
+        <div className="descCard">
         <h2>{card.title}</h2>
         <p>{card.desc}</p>
         <a href={card.link} target="_blank" rel="noopener noreferrer">
           Learn more
         </a>
+        </div>
       </div>
     ))}
   </div>
