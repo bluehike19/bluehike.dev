c92b630474f2fd149a274bcdd831552860e17275
simo blues <wambuisimon712@gmail.com>
2024-01-19 23:47:49 +0300

    changes

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -4,11 +4,12 @@ import './home.scss'
 
 const home = () => {
   return (
+    <div>
     <div className='projectcard' data-aos="zoom-in-up">
     {sampleProjectData.map((card) => (
-      <div key={card.id} className='cards'>
+        <div key={card.id} className='cards'>
         <div className='imgCard'>
-          <img src={card.img} alt={card.title} />
+          <img src={card.img} alt={card.title} />  
         </div>
         <div className='descriptionCard'>
           <p></p>
@@ -16,6 +17,7 @@ const home = () => {
         </div>
       </div>
     ))}
+    </div>
     </div>
   )
 }
