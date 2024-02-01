9bbed0fdbc936a23a66afb3d26a6fa065cd67f44
simo blues <wambuisimon712@gmail.com>
2024-02-01 05:32:41 +0300

    changes

--- api/routes/comments.js
+++ api/routes/comments.js
@@ -1,3 +1,27 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const Post = require('../models/Post')
+const Comment = require('../models/Comment')
+
+//CREATE
+router.post('/create', async (req, res) => {
+    try {
+        const newComment = new Comment (req.body)
+        const savedComment = await newComment.save()
+        res.status(200).json(savedComment)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//UPDATE
+router.delete('/:id', async (req, res) => {
+    try {
+        const updatedComment = await Comment.findByIdAndUpdate(req.params.id, {$set:req.body}, {new: true})
+        res.status(200).json(updatedComment)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
