87ac96185c835b85de551c2453cd3f210c7b5f7e
simo blues <wambuisimon712@gmail.com>
2024-02-02 06:01:23 +0300

    changes

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -13,3 +13,13 @@ const verifyToken = (req, res, next) => {
         return res.status(401).json("you are not authenticated!")
     }
 }
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("you are not allowed to do that!")
+        }
+    })
+}
