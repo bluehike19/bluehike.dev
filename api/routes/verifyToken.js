f2c88935bca7fed5c4dfb01fc782e0549a496673
simo blues <wambuisimon712@gmail.com>
2024-02-02 06:04:45 +0300

    changes

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -22,4 +22,18 @@ const verifyTokenAndAuthorization = (req, res, next) => {
             res.status(403).json("you are not allowed to do that!")
         }
     })
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if(req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("you are not allowed to do that")
+        }
+    })
+}
+
+module.exports = {
+    verifyTokenAndAdmin, verifyTokenAndAuthorization
 }
